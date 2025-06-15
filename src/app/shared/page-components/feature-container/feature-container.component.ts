7ac173bec782cd0666f336953591c8a896b0b283
kjeshang <kunal@zypgallery.ca>
2025-06-15 13:04:44 -0700

    Created a feature container component

---
+++ src/app/shared/page-components/feature-container/feature-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-container',
+  imports: [],
+  templateUrl: './feature-container.component.html',
+  styleUrl: './feature-container.component.scss'
+})
+export class FeatureContainerComponent {
+
+}
