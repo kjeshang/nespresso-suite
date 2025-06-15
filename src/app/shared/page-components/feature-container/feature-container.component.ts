a8a6b265c6f9d34fc1999e5bd23a9a64b260ff7d
kjeshang <kunal@zypgallery.ca>
2025-06-15 13:07:43 -0700

    Created nav bar and footer component.

--- src/app/shared/page-components/feature-container/feature-container.component.ts
+++ src/app/shared/page-components/feature-container/feature-container.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../ui-components/nav-bar/nav-bar.component';
+import { FooterComponent } from '../../ui-components/footer/footer.component';
 
 @Component({
   selector: 'app-feature-container',
-  imports: [],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './feature-container.component.html',
   styleUrl: './feature-container.component.scss'
 })
