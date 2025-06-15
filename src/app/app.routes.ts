2f4c4aa08da9261d430bce7cdc0c107a9b53effb
kjeshang <kunal@zypgallery.ca>
2025-06-15 13:17:57 -0700

    Imported dashboard page to app route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardPageComponent } from './apps/dashboard/dashboard-page/dashboard-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"", component:DashboardPageComponent
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
