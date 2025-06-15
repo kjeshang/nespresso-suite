49a20f6e674658f148c43da8c5799c63a816b26e
kjeshang <kunal@zypgallery.ca>
2025-06-15 13:51:02 -0700

    Imported toolbar module into navbar component

--- src/app/apps/dashboard/dashboard-page/dashboard-page.component.ts
+++ src/app/apps/dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [FeatureContainerComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
--- src/app/shared/ui-components/nav-bar/nav-bar.component.ts
+++ src/app/shared/ui-components/nav-bar/nav-bar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink],
+  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
