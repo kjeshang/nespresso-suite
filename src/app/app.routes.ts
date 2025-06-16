f2340c27d1635a93e7fe461b1ce9189a22b49cc5
kjeshang <kunal@zypgallery.ca>
2025-06-15 17:39:08 -0700

    Created basic navigation bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardPageComponent } from './apps/dashboard/dashboard-page/dashboard-page.component';
+import { CardReconciliationPageComponent } from './apps/card-reconciliation/card-reconciliation-page/card-reconciliation-page.component';
 
 export const routes: Routes = [
     {
         path:"", component:DashboardPageComponent
+    },
+    {
+        path:"card-reconciliation", component:CardReconciliationPageComponent
     }
 ];
 
--- src/app/shared/ui-components/nav-bar/nav-bar.component.ts
+++ src/app/shared/ui-components/nav-bar/nav-bar.component.ts
@@ -3,10 +3,11 @@ import { RouterLink } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
