283ff6e8d4f3df82411a51a3c040e3f61005b854
kjeshang <kunal@zypgallery.ca>
2025-06-27 12:48:11 -0700

    Added remaining sales desks to the the db service function.

--- src/app/apps/card-reconciliation/card-reconciliation.db.service.ts
+++ src/app/apps/card-reconciliation/card-reconciliation.db.service.ts
@@ -43,6 +43,78 @@ export class CardReconciliationDbService {
                 },
                 difference: 0,
                 outcome: "Balanced"
+            },
+            D: {
+                salesDesk: 'D',
+                tillNumber: 4,
+                pos: {
+                    label: "POS Amount D",
+                    posAmount: 0,
+                },
+                register: {
+                    label: "Register Amount D",
+                    registerAmount: 0
+                },
+                terminal: {
+                    label: "Terminal Amount D",
+                    terminalAmount: 0
+                },
+                difference: 0,
+                outcome: "Balanced"
+            },
+            E: {
+                salesDesk: 'E',
+                tillNumber: 7,
+                pos: {
+                    label: "POS Amount E",
+                    posAmount: 0,
+                },
+                register: {
+                    label: "Register Amount E",
+                    registerAmount: 0
+                },
+                terminal: {
+                    label: "Terminal Amount E",
+                    terminalAmount: 0
+                },
+                difference: 0,
+                outcome: "Balanced"
+            },
+            F: {
+                salesDesk: 'F',
+                tillNumber: 6,
+                pos: {
+                    label: "POS Amount F",
+                    posAmount: 0,
+                },
+                register: {
+                    label: "Register Amount F",
+                    registerAmount: 0
+                },
+                terminal: {
+                    label: "Terminal Amount F",
+                    terminalAmount: 0
+                },
+                difference: 0,
+                outcome: "Balanced"
+            },
+            G: {
+                salesDesk: 'G',
+                tillNumber: 5,
+                pos: {
+                    label: "POS Amount G",
+                    posAmount: 0,
+                },
+                register: {
+                    label: "Register Amount G",
+                    registerAmount: 0
+                },
+                terminal: {
+                    label: "Terminal Amount G",
+                    terminalAmount: 0
+                },
+                difference: 0,
+                outcome: "Balanced"
             }
         };
         return storeConfiguration;
