0dd27db1954c38e83e4fa501edb73fb8bf5a1c3f
kjeshang <kunal@zypgallery.ca>
2025-07-03 16:36:13 -0700

    Created comments text area.

--- src/app/apps/card-reconciliation/card-reconciliation-summary/card-reconciliation-summary.component.ts
+++ src/app/apps/card-reconciliation/card-reconciliation-summary/card-reconciliation-summary.component.ts
@@ -1,10 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { CardReconciliationStore } from '../card-reconciliation.store';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { TextFieldModule } from '@angular/cdk/text-field';
+
 
 @Component({
   selector: 'app-card-reconciliation-summary',
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, TextFieldModule],
   templateUrl: './card-reconciliation-summary.component.html',
   styleUrl: './card-reconciliation-summary.component.scss'
 })
