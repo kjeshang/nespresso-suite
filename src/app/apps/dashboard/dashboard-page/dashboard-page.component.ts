eeb88e35cdda7e9e5ff20f121e3b3795e36b2066
kjeshang <kunal@zypgallery.ca>
2025-07-03 12:51:31 -0700

    Imported dashboard legacy and dashboard apps component in dashboard page.

--- src/app/apps/dashboard/dashboard-page/dashboard-page.component.ts
+++ src/app/apps/dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
+import { DashboardLegacyComponent } from '../dashboard-legacy/dashboard-legacy.component';
+import { DashboardAppsComponent } from '../dashboard-apps/dashboard-apps.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [FeatureContainerComponent],
+  imports: [FeatureContainerComponent, DashboardLegacyComponent, DashboardAppsComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
