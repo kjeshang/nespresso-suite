fb63f85c25873ea58c455164410ecacbec3a27ca
kjeshang <kunal@zypgallery.ca>
2025-07-11 12:53:46 -0700

    Added cash reconciliation app to the dashboard icon.

--- src/app/apps/cash-reconciliation/cash-reconciliation-page/cash-reconciliation-page.component.ts
+++ src/app/apps/cash-reconciliation/cash-reconciliation-page/cash-reconciliation-page.component.ts
@@ -18,4 +18,8 @@ export class CashReconciliationPageComponent {
     this.cashReconciliationTableComponent.resetCashReconciliationForm();
     this.cashReconciliationSummaryComponent.onSelectShift();
   }
+
+  printPage(): void {
+    window.print();
+  }
 }
