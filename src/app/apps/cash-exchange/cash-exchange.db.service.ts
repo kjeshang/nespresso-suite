7ba36d78dd3c775e9f17a51319e940847fa05c81
kjeshang <kunal@zypgallery.ca>
2025-07-22 16:24:59 -0700

    Added remaining denominations to db service functions.

--- src/app/apps/cash-exchange/cash-exchange.db.service.ts
+++ src/app/apps/cash-exchange/cash-exchange.db.service.ts
@@ -19,6 +19,54 @@ export class CashExchangeDbService {
                 denomination: 50,
                 countOfBillCoin: 0,
                 cashValue: 0,
+            },
+            twenties: {
+                denominationType: 'Bill',
+                denomination: 20,
+                countOfBillCoin: 0,
+                cashValue: 0,
+            },
+            tens: {
+                denominationType: 'Bill',
+                denomination: 10,
+                countOfBillCoin: 0,
+                cashValue: 0,
+            },
+            fives: {
+                denominationType: 'Bill',
+                denomination: 5,
+                countOfBillCoin: 0,
+                cashValue: 0,
+            },
+            toonies: {
+                denominationType: 'Coin',
+                denomination: 2,
+                countOfBillCoin: 0,
+                cashValue: 0,
+            },
+            loonies: {
+                denominationType: 'Coin',
+                denomination: 1,
+                countOfBillCoin: 0,
+                cashValue: 0,
+            },
+            quarters: {
+                denominationType: 'Coin',
+                denomination: 0.25,
+                countOfBillCoin: 0,
+                cashValue: 0,
+            },
+            dimes: {
+                denominationType: 'Coin',
+                denomination: 0.10,
+                countOfBillCoin: 0,
+                cashValue: 0,
+            },
+            nickels: {
+                denominationType: 'Coin',
+                denomination: 0.05,
+                countOfBillCoin: 0,
+                cashValue: 0,
             }
         }
         return moneySentOut;
@@ -38,6 +86,62 @@ export class CashExchangeDbService {
                 denomination: 50,
                 countOfBillRollsOfCoin: 0,
                 amountPerRoll: undefined,
+                cashValue: 0
+            },
+            twenties: {
+                denominationType: 'Bill',
+                denomination: 20,
+                countOfBillRollsOfCoin: 0,
+                amountPerRoll: undefined,
+                cashValue: 0
+            },
+            tens: {
+                denominationType: 'Bill',
+                denomination: 10,
+                countOfBillRollsOfCoin: 0,
+                amountPerRoll: undefined,
+                cashValue: 0
+            },
+            fives: {
+                denominationType: 'Bill',
+                denomination: 5,
+                countOfBillRollsOfCoin: 0,
+                amountPerRoll: undefined,
+                cashValue: 0
+            },
+            toonies: {
+                denominationType: 'Coin',
+                denomination: 2,
+                countOfBillRollsOfCoin: 0,
+                amountPerRoll: 50,
+                cashValue: 0
+            },
+            loonies: {
+                denominationType: 'Coin',
+                denomination: 1,
+                countOfBillRollsOfCoin: 0,
+                amountPerRoll: 25,
+                cashValue: 0
+            },
+            quarters: {
+                denominationType: 'Coin',
+                denomination: 0.25,
+                countOfBillRollsOfCoin: 0,
+                amountPerRoll: 10,
+                cashValue: 0
+            },
+            dimes: {
+                denominationType: 'Coin',
+                denomination: 0.10,
+                countOfBillRollsOfCoin: 0,
+                amountPerRoll: 5,
+                cashValue: 0
+            },
+            nickels: {
+                denominationType: 'Coin',
+                denomination: 0.05,
+                countOfBillRollsOfCoin: 0,
+                amountPerRoll: 2,
                 cashValue: 0
             }
         };
