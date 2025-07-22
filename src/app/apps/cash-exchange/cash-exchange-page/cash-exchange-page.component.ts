6e506b0b86e9508a6dbdfbafe70d28d946748a5c
kjeshang <kunal@zypgallery.ca>
2025-07-22 16:29:12 -0700

    Added feature container to page component.

--- src/app/apps/cash-exchange/cash-exchange-page/cash-exchange-page.component.ts
+++ src/app/apps/cash-exchange/cash-exchange-page/cash-exchange-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
 
 @Component({
   selector: 'app-cash-exchange-page',
-  imports: [],
+  imports: [FeatureContainerComponent],
   templateUrl: './cash-exchange-page.component.html',
   styleUrl: './cash-exchange-page.component.scss'
 })
