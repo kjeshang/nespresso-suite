abab04cad9cb11eed663450c5cd1387ccc608430
kjeshang <kunal@zypgallery.ca>
2025-08-07 22:35:46 -0700

    Started to create list of panels.

--- src/app/apps/coffee/coffee-recommendation/coffee-recommendation.component.ts
+++ src/app/apps/coffee/coffee-recommendation/coffee-recommendation.component.ts
@@ -3,10 +3,11 @@ import { CoffeeStore } from '../coffee.store';
 import { CommonModule } from '@angular/common';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-coffee-recommendation',
-  imports: [CommonModule, ScrollingModule, MatExpansionModule],
+  imports: [CommonModule, ScrollingModule, MatExpansionModule, MatTabsModule],
   templateUrl: './coffee-recommendation.component.html',
   styleUrl: './coffee-recommendation.component.scss'
 })
