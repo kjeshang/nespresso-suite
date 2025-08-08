80ddc0f1302c021c94d89ad18486d5bcc9edb6c1
kjeshang <kunal@zypgallery.ca>
2025-08-07 17:56:10 -0700

    Added models.

---
+++ src/app/apps/coffee/coffee-page/coffee-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee-page',
+  imports: [],
+  templateUrl: './coffee-page.component.html',
+  styleUrl: './coffee-page.component.scss'
+})
+export class CoffeePageComponent {
+
+}
---
+++ src/app/apps/coffee/coffee.models.ts
@@ -0,0 +1,57 @@
+export interface Coffee {
+    id: string;
+    name: string;
+    capsuleImage: string;
+    sleeveImage: string;
+    type: string;
+    status: string;
+    category: string;
+    cupSize: string;
+    headline: string;
+    intensity: number | string;
+    price: number;
+    notes: string;
+    taste: string;
+    tasteProfile: TasteProfile;
+    description: string;
+    origin: string;
+    roasting: string;
+    contentsAllergens: string;
+    ingredients: string;
+    netWeight: string;
+    uniqueName: string;
+    estimatedIntensity: number | string;
+    acidityClassification: string;
+    bitternessClassification: string;
+    roastinessClassification: string;
+    bodyClassification: string;
+    milkyTasteClassification: string;
+    bitternessWithMilkClassification: string;
+    roastinessWithMilkClassification: string;
+    creamyTextureClassification: string;
+    tasteProfileChart: string;
+    textualInfo: string;
+    wordCloud: string;
+    featureResults: string;
+    guide: string;
+    recmendations: Recommendation[];
+}
+
+export interface TasteProfile {
+    acidity: number;
+    bitterness: number;
+    roastiness: number;
+    body: number;
+    milkyTaste: number;
+    bitternessWithMilk: number;
+    roastinessWithMilk: number;
+    creamyTexture: number;
+}
+
+export interface Recommendation {
+    uniqueName: string;
+    type: string;
+    cupSize: string;
+    intensity: number | string;
+    similarityScore: number;
+}
