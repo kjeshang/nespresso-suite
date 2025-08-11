38b0bb951c0b2f0bda729336ce70e0a8224fe969
kjeshang <kunal@zypgallery.ca>
2025-08-10 19:28:47 -0700

    Added link to reference guide

--- src/app/apps/coffee/coffee-recommendation/coffee-recommendation.component.ts
+++ src/app/apps/coffee/coffee-recommendation/coffee-recommendation.component.ts
@@ -5,10 +5,12 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-coffee-recommendation',
-  imports: [CommonModule, ScrollingModule, MatExpansionModule, MatTabsModule, MatProgressSpinnerModule],
+  imports: [CommonModule, ScrollingModule, MatExpansionModule, MatTabsModule, MatProgressSpinnerModule, MatButtonModule, MatIconModule],
   templateUrl: './coffee-recommendation.component.html',
   styleUrl: './coffee-recommendation.component.scss'
 })
