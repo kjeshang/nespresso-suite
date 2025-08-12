0ada7efe1ad0bd73d6b90536d6baa64e272d19ea
kjeshang <kunal@zypgallery.ca>
2025-08-11 21:01:44 -0700

    Added status filter.

--- src/app/apps/coffee/coffee.store.ts
+++ src/app/apps/coffee/coffee.store.ts
@@ -8,12 +8,14 @@ type CoffeeState = {
     coffeeData: Coffee[];
     isLoading: boolean;
     query: string;
+    selectedStatus: string[];
 }
 
 const initialCoffeeState: CoffeeState = {
     coffeeData: [],
     isLoading: false,
     query: '',
+    selectedStatus: ['Current'],
 }
 
 export const CoffeeStore = signalStore(
@@ -35,19 +37,34 @@ export const CoffeeStore = signalStore(
                 query: query,
             }));
         },
+        async updateSelectedStatus(selectedStatus: string[]): Promise<void> {
+            patchState(store, (state: CoffeeState) => ({
+                selectedStatus: selectedStatus,
+            }));
+        },
     })),
     withComputed(
         (
             {
                 coffeeData,
                 query,
+                selectedStatus,
             }
         ) => ({
         filteredCoffeeData: computed(() => {
             let data: Coffee[] = chain(coffeeData())
             .filter((el: Coffee) => el.name.toLowerCase().includes(query().toLowerCase()))
+            .filter((el: Coffee) => selectedStatus().includes(el.status))
             .value();
             return data;
-        })
+        }),
+        uniqueStatus: computed(() => {
+            const uniqueStatus: string[] = chain(coffeeData())
+            .map((item: Coffee) => item.status)
+            .uniq()
+            .sort()
+            .value();
+            return uniqueStatus;
+        }),
     }))
 );
