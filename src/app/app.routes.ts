d58c574f5f25fed5a2e0ed0df98a8407bae02daa
kjeshang <kunal@zypgallery.ca>
2025-08-27 18:49:10 -0700

    Added first aid guide in iFrame.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { CardReconciliationPageComponent } from './apps/card-reconciliation/card
 import { CashReconciliationPageComponent } from './apps/cash-reconciliation/cash-reconciliation-page/cash-reconciliation-page.component';
 import { CashExchangePageComponent } from './apps/cash-exchange/cash-exchange-page/cash-exchange-page.component';
 import { CoffeePageComponent } from './apps/coffee/coffee-page/coffee-page.component';
+import { FirstAidPageComponent } from './apps/first-aid/first-aid-page/first-aid-page.component';
 
 export const routes: Routes = [
     {
@@ -21,7 +22,10 @@ export const routes: Routes = [
     },
     {
         path:"coffee", component:CoffeePageComponent
-    }
+    },
+    {
+        path:"first-aid", component:FirstAidPageComponent
+    },
 ];
 
 @NgModule({
--- src/app/apps/first-aid/first-aid-page/first-aid-page.component.ts
+++ src/app/apps/first-aid/first-aid-page/first-aid-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-first-aid-page',
-  imports: [],
+  imports: [FeatureContainerComponent, PdfViewerModule],
   templateUrl: './first-aid-page.component.html',
   styleUrl: './first-aid-page.component.scss'
 })
 export class FirstAidPageComponent {
+  filePath: string = 'data/First Aid Matrix Guide.pdf'
 
+  pdfUrl!: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    // Path must be relative to /src/assets/
+    this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.filePath);
+  }
 }
