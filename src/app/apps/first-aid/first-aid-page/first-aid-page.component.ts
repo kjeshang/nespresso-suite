bc049441a56c6789e98d5cb50b3bb2e0357a3391
kjeshang <kunal@zypgallery.ca>
2025-08-27 19:07:13 -0700

    Injected sanitizer using new angular.

--- src/app/apps/first-aid/first-aid-page/first-aid-page.component.ts
+++ src/app/apps/first-aid/first-aid-page/first-aid-page.component.ts
@@ -1,21 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
-import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-first-aid-page',
-  imports: [FeatureContainerComponent, PdfViewerModule],
+  imports: [FeatureContainerComponent],
   templateUrl: './first-aid-page.component.html',
   styleUrl: './first-aid-page.component.scss'
 })
 export class FirstAidPageComponent {
   filePath: string = 'data/First Aid Matrix Guide.pdf'
 
-  pdfUrl!: SafeResourceUrl;
-
-  constructor(private sanitizer: DomSanitizer) {
-    // Path must be relative to /src/assets/
-    this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.filePath);
-  }
+  sanitizer: DomSanitizer = inject(DomSanitizer);
+  pdfUrl: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.filePath);
 }
