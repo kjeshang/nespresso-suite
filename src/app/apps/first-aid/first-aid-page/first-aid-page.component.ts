33f36dede456ffe78690d8b00fbe4b3a05f59daa
kjeshang <kunal@zypgallery.ca>
2025-08-30 09:26:00 -0700

    Added button to show first aid guide as iframe does not render on mobile.

--- src/app/apps/first-aid/first-aid-page/first-aid-page.component.ts
+++ src/app/apps/first-aid/first-aid-page/first-aid-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-first-aid-page',
-  imports: [FeatureContainerComponent],
+  imports: [FeatureContainerComponent, MatButtonModule],
   templateUrl: './first-aid-page.component.html',
   styleUrl: './first-aid-page.component.scss'
 })
