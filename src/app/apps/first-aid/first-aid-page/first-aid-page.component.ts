c080536b4299adf71176fc6e964d906c27b2b08a
kjeshang <kunal@zypgallery.ca>
2025-09-02 19:07:50 -0700

    Changed mobile pdf link

--- src/app/apps/first-aid/first-aid-page/first-aid-page.component.ts
+++ src/app/apps/first-aid/first-aid-page/first-aid-page.component.ts
@@ -11,6 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class FirstAidPageComponent {
   filePath: string = 'data/First Aid Matrix Guide.pdf'
+  dropboxUrl: string = 'https://www.dropbox.com/scl/fi/cntqfwcxsvakmgaovhey7/First-Aid-Matrix-Guide.pdf?rlkey=fl6ni7rnis5njzug7buubalc8&e=1&dl=0';
 
   sanitizer: DomSanitizer = inject(DomSanitizer);
   pdfUrl: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.filePath);
