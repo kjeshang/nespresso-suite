30cdcf808af227f58c6dbd30ecdfc188b9c0186f
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-03-10 13:41:54 -0700

    Machine filters added
    Created machine filter component and incorporated filters

--- src/app/apps/machine/machine-filter/machine-filter.component.ts
+++ src/app/apps/machine/machine-filter/machine-filter.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MachineStore } from '../machine.store';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-machine-filter',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatSelectModule],
   templateUrl: './machine-filter.component.html',
   styleUrl: './machine-filter.component.scss'
 })
 export class MachineFilterComponent {
+  @Input() lastUpdated!: string;
 
+  private fb: FormBuilder = inject(FormBuilder);
+  machineStore = inject(MachineStore);
+  showFilterOptions: boolean = false;
+
+  machineFilterForm: FormGroup = this.fb.group({
+    query: [''],
+    selectedTypes: [this.machineStore.uniqueTypes()],
+  });
+
+  toggleFilterOptions(): void {
+    this.showFilterOptions = !this.showFilterOptions;
+  }
+
+  onQueryInput(): void {
+    const query: string = this.machineFilterForm.get('query')?.value;
+    this.machineStore.updateQuery(query);
+  }
+
+  onMachineTypeChange(event: MatCheckboxChange, item: string): void {
+    const selectedTypes: Set<string> = new Set<string>(this.machineFilterForm.get('selectedTypes')?.value);
+    if(event.checked) {
+      selectedTypes.add(item);
+    }
+    else {
+      selectedTypes.delete(item);
+    }
+    this.machineFilterForm.patchValue({selectedTypes: Array.from(selectedTypes)});
+    this.machineStore.updateSelectedTypes(this.machineFilterForm.get('selectedTypes')?.value);
+  }
 }
--- src/app/apps/machine/machine-page/machine-page.component.ts
+++ src/app/apps/machine/machine-page/machine-page.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
 import { MachineListComponent } from '../machine-list/machine-list.component';
+import { MachineFilterComponent } from '../machine-filter/machine-filter.component';
 
 @Component({
   selector: 'app-machine-page',
-  imports: [FeatureContainerComponent, MachineListComponent],
+  imports: [FeatureContainerComponent, MachineListComponent, MachineFilterComponent],
   templateUrl: './machine-page.component.html',
   styleUrl: './machine-page.component.scss'
 })
 export class MachinePageComponent {
-
+  lastUpdated: string = "November 2024";
 }
