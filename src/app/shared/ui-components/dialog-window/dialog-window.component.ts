bcc6116f387c5a15f97ac51a5b0ceb490ee11675
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-09-06 11:56:19 -0700

    generated dialog window component

---
+++ src/app/shared/ui-components/dialog-window/dialog-window.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-window',
+  imports: [],
+  templateUrl: './dialog-window.component.html',
+  styleUrl: './dialog-window.component.scss'
+})
+export class DialogWindowComponent {
+
+}
